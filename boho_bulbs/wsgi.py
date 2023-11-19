5c36af79f38e0861654e626b788a902400a49704
Abby Humphreys <bibby3316@gmail.com>
2023-11-19 08:08:30 +0000

    initial commit

---
+++ boho_bulbs/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for boho_bulbs project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boho_bulbs.settings')
+
+application = get_wsgi_application()
