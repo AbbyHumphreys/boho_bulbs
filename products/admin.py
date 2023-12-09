46c2ea72d204c3921e3a26652a513d7faf43e150
Abby Humphreys <bibby3316@gmail.com>
2023-12-09 18:51:10 +0000

    add: products models to admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product, Brand, Colour, IpRating, LampType
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Brand)
+admin.site.register(Colour)
+admin.site.register(IpRating)
+admin.site.register(LampType)
