a1a107116c7f7fdf74a6396dd82a3bef2bfb4695
Abby Humphreys <bibby3316@gmail.com>
2023-12-17 18:56:24 +0000

    add: functionality for sorting and direction of sorting

--- products/views.py
+++ products/views.py
@@ -10,8 +10,23 @@ def all_products(request):
     query = None
     lamptype = None
     colour = None
+    sort = None
+    direction = None
 
     if request.GET:
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction =='desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
         if 'lamptype' in request.GET:
             lamptype = request.GET['lamptype'].split(',')
@@ -32,11 +47,14 @@ def all_products(request):
             queries = Q(lamptitle__icontains=query) | Q(description1__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_lamptype': lamptype,
         'current_colour':colour,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
