d1e902a36e753336f6d3e7c77ff7d9b838d0370e
Abby Humphreys <bibby3316@gmail.com>
2023-12-17 18:45:47 +0000

    add: filters for lamptype and colours

--- products/views.py
+++ products/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, LampType, Colour
 
 def all_products(request):
     """ A view to show all products, including sorting and searching """
 
     products = Product.objects.all()
     query = None
+    lamptype = None
+    colour = None
 
     if request.GET:
+
+        if 'lamptype' in request.GET:
+            lamptype = request.GET['lamptype'].split(',')
+            products = products.filter(lamptype__name__in=lamptype)
+            lamptype = LampType.objects.filter(name__in=lamptype)
+
+        if 'colour' in request.GET:
+            colour = request.GET['colour'].split(',')
+            products = products.filter(colour__name__in=colour)
+            colour = Colour.objects.filter(name__in=colour)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -21,6 +34,9 @@ def all_products(request):
 
     context = {
         'products': products,
+        'search_term': query,
+        'current_lamptype': lamptype,
+        'current_colour':colour,
     }
 
     return render(request, 'products/products.html', context)
