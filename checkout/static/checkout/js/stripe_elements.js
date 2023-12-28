00348c14a16c83263526fe7f08a1bf7859f71b36
Abby Humphreys <bibby3316@gmail.com>
2023-12-28 11:32:29 +0000

    add: stripe basic functionality

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,30 @@
+let stripe_public_key = document.getElementById('id_stripe_public_key').textContent.slice(1, -1);
+
+var stripe = Stripe(stripe_public_key);
+
+  // Create an instance of Elements.
+  var elements = stripe.elements();
+
+  // create some style for the card element
+  var style = {
+    base: {
+      color: '#32325d',
+      lineHeight: '18px',
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: 'antialiased',
+      fontSize: '16px',
+      '::placeholder': {
+        color: '#aab7c4'
+      }
+    },
+    invalid: {
+      color: '#fa755a',
+      iconColor: '#fa755a'
+    }
+  };
+
+  // Create an instance of the card Element.
+  var card = elements.create('card', {style: style});
+
+  // Add an instance of the card Element into the `card-element` <div>.
+  card.mount('#card-element');
