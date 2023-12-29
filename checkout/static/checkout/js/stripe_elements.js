b7d442012c2f1b702b3994d1a2c7d150b2e4c69c
Abby Humphreys <bibby3316@gmail.com>
2023-12-29 10:18:34 +0000

    add: functionality to handle stripe form submission

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,12 +1,12 @@
-let stripe_public_key = document.getElementById('id_stripe_public_key').textContent.slice(1, -1);
+let stripePublicKey = document.getElementById('id_stripe_public_key').textContent.slice(1, -1);
 
-var stripe = Stripe(stripe_public_key);
+let stripe = Stripe(stripePublicKey);
 
   // Create an instance of Elements.
-  var elements = stripe.elements();
+  let elements = stripe.elements();
 
   // create some style for the card element
-  var style = {
+  let style = {
     base: {
       color: '#32325d',
       lineHeight: '18px',
@@ -24,17 +24,64 @@ var stripe = Stripe(stripe_public_key);
   };
 
   // Create an instance of the card Element.
-  var card = elements.create('card', {style: style});
+  let card = elements.create('card', {style: style});
 
   // Add an instance of the card Element into the `card-element` <div>.
   card.mount('#card-element');
 
   // Handle real-time validation errors from the card Element.
   card.addEventListener('change', function(event) {
-    var displayError = document.getElementById('card-errors');
+    let displayError = document.getElementById('card-errors');
     if (event.error) {
       displayError.textContent = event.error.message;
     } else {
       displayError.textContent = '';
     }
   });
+
+// Handle form submission.
+let form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Disable the card and the submit button
+  card.update({ 'disabled': true});
+  document.getElementById('submit-button').disabled = true;
+
+  stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+          card: card,
+      }
+  })
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error.
+      let errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+      // Re-enable the card and the submit button
+      card.update({ 'disabled': false});
+      document.getElementById('submit-button').disabled = false;
+    } else {
+        if (result.paymentIntent.status === 'succeeded') {
+            // Submit the form
+            form.submit();
+        }
+  }});
+});
+
+let successElement = document.getElementById('stripe-token-handler');
+
+function stripeTokenHandler(token) {
+  successElement.className = '';
+  successElement.querySelector('.token').textContent = token.id;
+  // Insert the token ID into the form so it gets submitted to the server
+  let form = document.getElementById('payment-form');
+  let hiddenInput = document.createElement('input');
+  hiddenInput.setAttribute('type', 'hidden');
+  hiddenInput.setAttribute('name', 'stripeToken');
+  hiddenInput.setAttribute('value', token.id);
+  form.appendChild(hiddenInput);
+
+  // Submit the form
+  form.submit();
+}
