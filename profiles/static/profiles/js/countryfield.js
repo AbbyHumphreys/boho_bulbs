eb9c50691751e9d5a2d7f512d7ca11ed92de4612
Abby Humphreys <bibby3316@gmail.com>
2023-12-31 16:40:23 +0000

    style: country field in profile view

---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,13 @@
+let countrySelected = document.getElementById('id_default_country').value;
+if(!countrySelected) {
+    document.getElementById('id_default_country').style.color = '#aab7c4'
+};
+
+document.getElementById('id_default_country').addEventListener('change', function() {
+    countrySelected = this.value;
+    if (!countrySelected) {
+        this.style.color = '#aab7c4';
+    } else {
+        this.style.color = '#000';
+    }
+});
