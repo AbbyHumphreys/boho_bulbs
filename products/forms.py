eaf06ab7c2c222f82cdc8ff543147ea3466227b9
Abby Humphreys <bibby3316@gmail.com>
2024-01-01 09:06:52 +0000

    fix: typos in add product functionality

--- products/forms.py
+++ products/forms.py
@@ -15,14 +15,14 @@ class ProductForm(forms.ModelForm):
         ipratings = IpRating.objects.all()
         brands = Brand.objects.all()
         colours_friendly_names = [(c.id, c.get_friendly_name()) for c in colours]
-        lamptypes_friendly_names = [(c.id, c.get_friendly_name()) for l in lamptypes]
-        ipratings_friendly_names = [(c.id, c.get_friendly_name()) for i in ipratings]
-        brands_friendly_names = [(c.id, c.get_friendly_name()) for b in brands]
+        lamptypes_friendly_names = [(l.id, l.get_friendly_name()) for l in lamptypes]
+        ipratings_friendly_names = [(i.id, i.get_friendly_name()) for i in ipratings]
+        brands_friendly_names = [(b.id, b.get_friendly_name()) for b in brands]
 
 
-        self.fields['colours'].choices = friendly_names
-        self.fields['lamptypes'].choices = friendly_names
-        self.fields['ipratings'].choices = friendly_names
-        self.fields['brands'].choices = friendly_names
+        self.fields['colour'].choices = colours_friendly_names
+        self.fields['lamptype'].choices = lamptypes_friendly_names
+        self.fields['iprating'].choices = ipratings_friendly_names
+        self.fields['brand'].choices = brands_friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'rounded-0'
