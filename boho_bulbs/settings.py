d048143ed76915ed46deb51bbad5adc3a4a0d1bc
Abby Humphreys <bibby3316@gmail.com>
2024-01-07 10:36:06 +0000

    amend: debug typo

--- boho_bulbs/settings.py
+++ boho_bulbs/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
 
-DEBGUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get('LOCALHOST'), 'boho-bulbs-12038623272a.herokuapp.com']
 
