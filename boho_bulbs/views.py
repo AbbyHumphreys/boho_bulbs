a8532a965ec55b546e76bd0fa5257c662e8f37f0
Abby Humphreys <bibby3316@gmail.com>
2024-01-13 15:33:53 +0000

    add: custom error pages

---
+++ boho_bulbs/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def handler404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "errors/404.html", status=404)
+
+
+def handler403(request, exception):
+    """Rendering the 403 page."""
+    if isinstance(exception, PermissionDenied):
+        return render(request, 'errors/403.html', status=403)
+    else:
+        return render(request, 'errors/500.html', status=500)
